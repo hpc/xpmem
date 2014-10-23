b147d4c56c480cf426dc0ce28b953515c54961f5
Nathan Hjelm <hjelmn@lanl.gov>
2014-10-23 09:21:00 -0700

    Autotools updates

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-autoreconf -i
+if (libtoolize --version) < /dev/null > /dev/null 2>&1; then
+  LIBTOOLIZE=libtoolize
+else
+  echo "libtoolize was not found! Please install libtool." 1>&2
+  exit 1
+fi
+
+$LIBTOOLIZE --copy --force || exit 1
+aclocal || exit 1
+autoheader || exit 1
+autoconf || exit 1
+automake -a -c || exit 1
